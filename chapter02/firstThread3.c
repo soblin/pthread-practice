261f99fbdd0e98547073a66cf40228a5ad9806fb
soblin <hilo.soblin@gmail.com>
2020-04-25 03:00:52 +0900

    pthread_joinしてメインの処理が終了したらスレッドの終了を待つようにした

--- chapter02/firstThread3.c
+++ chapter02/firstThread3.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct arg_t {
+  int val;
+} arg_t;
+
 void stopIfEqualsOne(int i){
   if(i == 1){
     printf("Hasta la vista, baby\n");
@@ -10,17 +14,27 @@ void stopIfEqualsOne(int i){
   }
 }
 void *threadWorker(void *arg){
-  for(int i=0; i<3; ++i){
+  arg_t *ptr = (arg_t *)arg;
+  const int cnt = ptr->val;
+  
+  for(int i=0; i<cnt; ++i){
     printf("I'm in threadWorker: i = %d\n", i);
-    stopIfEqualsOne(i);
     sleep(1);
   }
 }
 
 int main(int argc, char **argv){
+  if(argc <= 1){
+    printf("Usage: ./main <int>\n");
+    exit(1);
+  }
+
+  int cnt = atoi(argv[1]);
+  arg_t arg = {cnt};
+
   pthread_t thread;
 
-  if(pthread_create(&thread, NULL, threadWorker, NULL) != 0){
+  if(pthread_create(&thread, NULL, threadWorker, (void *)(&arg)) != 0){
     perror("pthread_create");
     exit(1);
   }
@@ -30,5 +44,13 @@ int main(int argc, char **argv){
     sleep(1);
   }
 
+  if(pthread_join(thread, NULL) != 0){
+    // stop this main thread until `thread` finishes.
+    perror("pthread_join");
+    exit(1);
+  }
+  
+  printf("Bye\n");
+  
   return 0;
-
+}
