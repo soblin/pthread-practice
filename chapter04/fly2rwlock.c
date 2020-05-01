ea5537e24bbeb24cc65a628e29c4b9f1771266b0
soblin <hilo.soblin@gmail.com>
2020-05-01 18:50:37 +0900

    read/write lock使った

--- chapter04/fly2rwlock.c
+++ chapter04/fly2rwlock.c
@@ -7,7 +7,7 @@
 #define WIDTH 78
 #define HEIGHT 23
 #define MAX_FLY 6
-static const char *flyMarkList = "o@*+.#";
+const char *flyMarkList = "o@*+.#";
 #define DRAW_CYCLE 50
 #define MIN_SPEED 1.0
 #define MAX_SPEED 20.0
@@ -38,36 +38,26 @@ typedef struct {
   double x, y;
   double angle;
   double speed;
-  pthread_mutex_t mutex;
+  pthread_rwlock_t rwlock;
 } Fly;
 
 Fly flyList[MAX_FLY];
 
 void FlyInitRandom(Fly *fly, char mark){
   fly->mark = mark;
+  pthread_rwlock_init(&fly->rwlock, NULL);
   fly->x = randDouble(0, (double)(WIDTH - 1));
   fly->y = randDouble(0, (double)(HEIGHT - 1));
   fly->angle = randDouble(0, M_2_PI);
   fly->speed = randDouble(MIN_SPEED, MAX_SPEED);
-  pthread_mutex_init(&fly->mutex, NULL);
 }
 
 void FlyDestroy(Fly *fly){
-  pthread_mutex_destroy(&fly->mutex);
-}
-
-double FlyDistance(Fly *fly, double x, double y){
-  pthread_mutex_lock(&fly->mutex);
-  double dx = x - (fly->x);
-  double dy = y - (fly->y);
-  double d = hypot(dx, dy);
-  pthread_mutex_unlock(&fly->mutex);
-  return d;
+  pthread_rwlock_destroy(&fly->rwlock);
 }
 
 void FlyMove(Fly *fly){
-  pthread_mutex_lock(&fly->mutex);
-
+  pthread_rwlock_wrlock(&fly->rwlock);
   fly->x += cos(fly->angle);
   fly->y += sin(fly->angle);
 
@@ -76,34 +66,26 @@ void FlyMove(Fly *fly){
     fly->angle = M_PI - (fly->angle);
   }
   else if(fly->x > (WIDTH - 1)){
-    fly->x = (WIDTH - 1);
+    fly->x = WIDTH - 1;
     fly->angle = M_PI - (fly->angle);
   }
+
   if(fly->y < 0){
     fly->y = 0;
     fly->angle = -(fly->angle);
   }
   else if(fly->y > (HEIGHT - 1)){
-    fly->y = (HEIGHT - 1);
+    fly->y = HEIGHT - 1;
     fly->angle = -(fly->angle);
   }
 
-  pthread_mutex_unlock(&fly->mutex);
-  
-  // 他のflyの近くにきたら方向を180度変える
-  for(int i = 0; i < MAX_FLY; ++i){
-    if((flyList[i].mark != fly->mark) && (FlyDistance(&flyList[i], fly->x, fly->y) < 2.0)){
-      // このスレッドのハエのangleを変更しようとしたときに別のスレッドがこのハエに対してFlyDistanceを呼び出したとしても，別にangleにはアクセスしないから，メモリレベルでは同時アクセスは起きていない，よってこの時点でunlockされていても問題ない
-      fly->angle += M_PI;
-      break;
-    }
-  }
+  pthread_rwlock_unlock(&fly->rwlock);
 }
 
 int FlyIsAt(Fly *fly, int x, int y){
-  pthread_mutex_lock(&fly->mutex);
+  pthread_rwlock_rdlock(&fly->rwlock);
   int res = ((int)(fly->x) == x) && ((int)(fly->y) == y);
-  pthread_mutex_unlock(&fly->mutex);
+  pthread_rwlock_unlock(&fly->rwlock);
   return res;
 }
 
@@ -130,9 +112,9 @@ void drawScreen(){
       }
       if(ch != 0)
         putchar(ch);
-      else if((y == 0) || (y == (HEIGHT - 1)))
+      else if((y == 0) || (y == HEIGHT-1))
         putchar('-');
-      else if((x == 0) || (x == (WIDTH - 1)))
+      else if((x == 0) || (x == WIDTH-1))
         putchar('|');
       else
         putchar(' ');
@@ -157,17 +139,14 @@ int main(){
 
   srand((unsigned int)time(NULL));
   clearScreen();
-
   for(int i = 0; i < MAX_FLY; ++i){
     FlyInitRandom(&flyList[i], flyMarkList[i]);
   }
 
   for(int i = 0; i < MAX_FLY; ++i){
-    if(0 != pthread_create(&moveThread[i], NULL, doMove, (void *)&flyList[i])){
-      perror("pthread_create");
-      exit(1);
-    }
+    pthread_create(&moveThread[i], NULL, doMove, (void *)&flyList[i]);
   }
+
   pthread_create(&drawThread, NULL, doDraw, NULL);
 
   fgets(buf, sizeof(buf), stdin);
